b217fc7aa4e16820adac9b40153b801005026254
kunalsoni <jp6714410@gmail.com>
2026-05-06 14:12:52 +0530

    deployment stage changes made

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,22 @@
-import express from 'express';
+import express from "express";
+import path from "path";
 const app = express();
-app.get("/api/health",(req,res)=>{
-    res.status(200).json({message:"server is running fine"});
-})
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
-})
+import {ENV} from "./config/env.js";
+const __dirname = path.resolve();
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "server is running fine" });
+});
+
+// making ready for the deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+
+    app.get("/{*any}", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../admin","dist","index.html"));
+    })
+}
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
