cb472587f27a5c8db7e0265ffef6be6211e0627c
kunalsoni <jp6714410@gmail.com>
2026-05-11 09:21:48 +0530

    add userId, orderId, and rating fields to review schema

--- backend/src/models/review.models.js
+++ backend/src/models/review.models.js
@@ -4,7 +4,24 @@ const reviewSchema = new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:"Product",
         required:true,
+     },
+     userId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User",
+      required:true,
+     },
+     orderId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"Order",
+      required:true,
+     },
+     rating:{
+      type:Number,
+      required:true,
+      min:1,
+      max:5
      }
+
 },{timestamps:true});
 
 export const Review  = mongoose.model('Review',reviewSchema);
