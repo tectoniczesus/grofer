d38d36cdeae21581987fd25a826d33b43914f2c0
kunalsoni <jp6714410@gmail.com>
2026-05-18 22:19:26 +0530

    feat: implement routing and authentication in App component; add axios instance for API calls

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,20 +1,27 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import React from 'react'
-
+import {Navigate, Route, Routes} from "react-router";
+import LoginPage from "./pages/LoginPage";
+import {useAuth} from "@clerk/react";
+import DashboardLayout from "./layout/DashboardLayout";
+import DashboardPage from "./pages/DashboardPage";
+import ProductsPage from "./pages/ProductsPage";
+import CustomersPage from "./pages/CustomersPage";
+import OrdersPage from "./pages/OrdersPage";
 function App() {
+  const {isSignedIn,isLoaded} = useAuth();
+   if(!isLoaded) return null;
   return (
-    <div>
-      <h1>home page</h1>
-       <header>
-        <Show when="signed-out">
-          <SignInButton />
-          <SignUpButton />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+
+    <Routes>
+     <Route path="/login" element = {isSignedIn ? <Navigate to={"/dashboard"}/>:<LoginPage/>}/>
+     <Route path="/" element={isSignedIn ? <DashboardLayout/> : <Navigate to={"/login"}/>}>
+       <Route index element={<Navigate to ={"dashboard"} />}/>
+       <Route path = "dashboard" element = {<DashboardPage/>} />
+       <Route path = "products" element = {<ProductsPage/>} />
+       <Route path = "customers" element = {<CustomersPage/>} />
+       <Route path = "orders" element = {<OrdersPage/>} />
+
+     </Route>
+    </Routes>
   )
 }
 
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {ClerkProvider} from "@clerk/react"
+import {BrowserRouter} from "react-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
 import App from './App.jsx'
 
 
@@ -9,10 +15,15 @@ const publishKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if(!publishKey){
   throw new Error("publish key is missing");
 }
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <ClerkProvider publishableKey={publishKey}>
+      <QueryClientProvider client={queryClient}>
       <App />
+      </QueryClientProvider>
     </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
