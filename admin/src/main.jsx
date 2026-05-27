c57c83ef85b9bdbd3892c66c85dce6ee79865e95
kunalsoni <jp6714410@gmail.com>
2026-05-27 14:17:20 +0530

    feat: integrate Sentry for error tracking and performance monitoring

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {ClerkProvider} from "@clerk/react"
 import {BrowserRouter} from "react-router";
+import * as Sentry from "@sentry/react";
 import {
   QueryClient,
   QueryClientProvider,
@@ -16,6 +17,21 @@ if(!publishKey){
   throw new Error("publish key is missing");
 }
 const queryClient = new QueryClient();
+
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  enableLogs:true,
+  integrations: [
+    Sentry.replayIntegration()
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
