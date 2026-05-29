5fc965d9e89e91af3c7f36f2e0a8432b8ad7af24
kunalsoni <jp6714410@gmail.com>
2026-05-29 14:23:52 +0530

    feat: enhance AuthScreen with social authentication options and loading indicators

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -4,6 +4,11 @@ import {useAuth} from "@clerk/expo";
 export default function AuthLayout() {
 
     const{isSignedIn} = useAuth()
+    // thing are working fine but 
+
+    // getting user and logging user in console too
+    // only maximum dept limit reached error is comming gonna need to fix that
+    
     if(isSignedIn){
         return <Redirect href={"/"}/>
     }
