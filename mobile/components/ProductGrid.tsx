d4349ac649020a8c2e93dddaf503e989e6b385ed
kunalsoni <jp6714410@gmail.com>
2026-06-01 19:57:40 +0530

    feat: implement ProductGrid component with cart functionality and loading/error states

--- mobile/components/ProductGrid.tsx
+++ mobile/components/ProductGrid.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, Image, ActivityIndicator, Alert } from 'react-native'
 import { Product } from '@/types';
 import useWishlist from '@/hooks/useWishlist';
+import { Ionicons } from '@expo/vector-icons';
+import { router } from '@/.expo/types/router';
+import useCart from '@/hooks/useCart';
 interface ProductGridProps{
   isLoading: boolean;
   isError: boolean;
@@ -9,12 +12,134 @@ interface ProductGridProps{
 
 const ProductGrid = ({products, isLoading, isError}: ProductGridProps) => {
   const {isInWishlist,toggleWishlist,isAddingToWishlist,isRemovingFromWishlist} = useWishlist();
-  
+  const{isAddingToCart , addToCart}= useCart();
+
+  const handleAddToCart = (productId:string, productName:string)=>{
+   addToCart(
+    {productId, quantity:1},
+    {
+      onSuccess:()=>{
+        Alert.alert("Success", `${productName} added to cart!`);
+      },
+      onError:(error:any)=>{
+        Alert.alert("Error",error?.response?.data?.error || "Failed to add product to cart. Please try again. ");
+      },
+    }
+   );
+  };
+
+
+  const renderProduct = ({item:product}:{item:Product})=>(
+    <TouchableOpacity
+    className = "bg-surface rounded-3xl overflow-hidden mb-3"
+    style={{width: '48%'}}
+    activeOpacity={0.8}
+    // onPress = {()=> router.push(`/product/${product._id}`)}
+    >
+    <View className = "relative">
+
+      <Image
+      source = {{uri: product.images[0]}}
+      className = "w-full h-44 bg-background-lighter"
+      resizeMode = "cover"
+      />
+
+      <TouchableOpacity
+      className = "absolute top-3 right-3 bg-black/30 backdrop-blur-xl p-2 rounded-full"
+      activeOpacity = {0.7}
+      onPress = {()=> toggleWishlist(product._id)}
+      disabled = {isAddingToWishlist || isRemovingFromWishlist}
+      >
+        {isAddingToWishlist || isRemovingFromWishlist ? (
+          
+            <ActivityIndicator size="small" color="#FFFFFF" />
+          
+        ) : (
+          
+            <Ionicons
+              name={isInWishlist(product._id) ? "heart" : "heart-outline"}
+              size={20}
+              color={isInWishlist(product._id) ? "#ff4444" : "#fff"}
+            />
+          
+        )}
+      </TouchableOpacity>
+    </View>
+
+    <View className = "p-3">
+      <Text className='text-text-secondary text-xs mb-1'>{product.category}</Text>
+      <Text className="text-text-primary font-bold text-sm mb-2">{product.name}</Text>
+     <View className = "flex-row items-center mb-2">
+       <Ionicons name="star" size={12} color="#FFD700"/>
+       <Text className = "text-text-primary text-xs font-semibold ml-1">
+        {product.averageRating.toFixed(1)}
+       </Text>
+       <Text className="text-text-secondary text-xs ml-1">
+        ({product.totalReviews})
+       </Text>
+     </View>
+     
+     <View className = "flex-row items-center justify-between">
+      <Text className = "text-primary font-bold text-lg">${product.price.toFixed(2)}</Text>
+      <TouchableOpacity
+      className = "bg-primary rounded-full w-8 h-8 items-center justify-center"
+      activeOpacity = {0.7}
+      onPress = {()=> handleAddToCart(product._id, product.name)}
+      disabled = {isAddingToCart}
+      >
+        {isAddingToCart ? (
+          <ActivityIndicator size="small" color="#121212"/>
+        ):(
+          <Ionicons name = "add" size={18} color = "#121212"/>
+        )}
+      </TouchableOpacity>
+     </View>
+    </View>
+    </TouchableOpacity>
+  );
+
+  if(isLoading){
+    return(
+      <View className = "py-20 items-center justify-center">
+        <ActivityIndicator size = "large" color="#00D9FF"/>
+        <Text className="text-text-primary mt-4">Loading products...</Text>
+      </View>
+    )
+  }
+
+  if(isError){
+    return(
+      <View className = "py-20 items-center justify-center">
+        <Ionicons name = "alert-circle-outline" size = {48} color = "#FF6B6B"/>
+        <Text className="text-text-primary font-semibold mt-4">Failed to load products</Text>
+        <Text className="text-text-secondary text-sm mt-2">Please try again later.</Text>
+      </View>
+    )
+  }
+    
   return (
-    <View>
-      <Text className='text-white'>ProductGrid</Text>
+    <FlatList
+      data={products}
+      renderItem={renderProduct}
+      keyExtractor={(item) => item._id}
+      numColumns = {2}
+      showsVerticalScrollIndicator = {false}
+      scrollEnabled = {false}
+      ListEmptyComponent ={NoProductFound}
+
+    />
+  );
+}
+
+export default ProductGrid;
+
+
+function NoProductFound(){
+  return(
+    <View className = "py-20 items-center justify-center">
+      <Ionicons name = "search-outline" size={48} color={"#666"}/>
+      <Text className = "text-text-primary font-semibold mt-4">No products found</Text>
+      <Text className = "text-text-secondary mt-2">Try adjusting your search terms</Text>
     </View>
   )
 }
-
-export default ProductGrid
