0755506c711780b9c91bfb81ffe6743de114f7a9
kunalsoni <jp6714410@gmail.com>
2026-06-03 16:04:52 +0530

    fix: update background color for privacy information alert

--- mobile/app/(profile)/privacy-security.tsx
+++ mobile/app/(profile)/privacy-security.tsx
@@ -263,7 +263,7 @@ function privacyAndSecurity(){
         </View>
         {/* INFO ALERT */}
         <View className="px-6 pt-6 pb-4">
-          <View className="bg-primary/10 rounded-2xl p-4 flex-row">
+          <View className="bg-green-800/20 rounded-2xl p-4 flex-row">
             <Ionicons name="information-circle-outline" size={24} color="#1DB954" />
             <Text className="text-text-secondary text-sm ml-3 flex-1">
               We take your privacy seriously. Your data is encrypted and stored securely. You can
